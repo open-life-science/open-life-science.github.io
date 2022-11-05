73c7ffb6bd1a4abafcac99a2c2a9957ce9fce27d
Tempestt Woodard <tnwoodard@gmail.com>
2022-11-05 14:31:18 +0000

    feat(searchbar):
    Issue #507
    
    Built the search function with Javascript. Will fix content that disappears upon onkeyup.

---
+++ js/posts.js
@@ -0,0 +1,14 @@
+// searchbar functionality
+function search_posts() {
+  let input = document.getElementById('searchbar').value
+  input = input.toLowerCase();
+  let x = document.getElementsByClassName('content')
+  for (i = 0; i < x.length; i++) {
+      if (!x[i].innerHTML.toLowerCase().includes(input)) {
+          x[i].style.display = "none";
+      }
+      else {
+          x[i].style.display= "inherit";
+      }
+  }
+};
