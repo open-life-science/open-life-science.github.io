52ebf8ba3e55ff4e9c8cda9653e3f798cda754d3
bebatut <bebatut@gmail.com>
2023-12-05 11:03:58 +0100

    Add possibility for other program in script and documentation

--- bin/openseeds/add_mentors_experts.sh
+++ bin/openseeds/add_mentors_experts.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
 python bin/prepare_website_data.py addmentorsexperts \
+    -p "openseeds"  \
     -c 6 \
     -t "mentor" \
     -du "https://docs.google.com/spreadsheets/d/1qZNk78paO54VNB_xvytuzKLlpcZtXMc6PezN4JMMcr8/export?format=csv&gid=1375310074"
 
 python bin/prepare_website_data.py addmentorsexperts \
+    -p "openseeds"  \
     -c 6 \
     -t "expert" \
     -du "https://docs.google.com/spreadsheets/d/1qZNk78paO54VNB_xvytuzKLlpcZtXMc6PezN4JMMcr8/export?format=csv&gid=1635903403"
--- bin/openseeds/update_schedule.sh
+++ bin/openseeds/update_schedule.sh
@@ -3,6 +3,7 @@
 echo "OLS-1"
 python bin/prepare_website_data.py \
     updateschedule \
+    -p "openseeds"  \
     --cohort '1' \
     --schedule_url "https://docs.google.com/spreadsheets/d/1BHXNCzxaBTITMLK9JWrZZMCM2pp77LP1HxS5jKwqhpM/export?format=csv&gid=0"
 echo
@@ -10,6 +11,7 @@ echo
 echo "OLS-2"
 python bin/prepare_website_data.py \
     updateschedule \
+    -p "openseeds"  \
     --cohort '2' \
     --schedule_url "https://docs.google.com/spreadsheets/d/1V8qy9-SmhVD-4q347n9W_fPOAk_gSFF_gz8rmYn79mU/export?format=csv&gid=0"
 echo
@@ -17,6 +19,7 @@ echo
 echo "OLS-3"
 python bin/prepare_website_data.py \
     updateschedule \
+    -p "openseeds"  \
     --cohort '3' \
     --schedule_url "https://docs.google.com/spreadsheets/d/1r0FqsosE6Y54jHLYy4nutlgprdxhchi993zUcvaAWwU/export?format=csv&gid=671805455"
 echo
@@ -24,6 +27,7 @@ echo
 echo "OLS-4"
 python bin/prepare_website_data.py \
     updateschedule \
+    -p "openseeds"  \
     --cohort '4' \
     --schedule_url "https://docs.google.com/spreadsheets/d/1-mNA_IcGYkr2b92QpGyD9qSeN5OC5T5d1p5hhM_fVYw/export?format=csv&gid=1181436982"
 echo
@@ -31,6 +35,7 @@ echo
 echo "OLS-5"
 python bin/prepare_website_data.py \
     updateschedule \
+    -p "openseeds"  \
     --cohort '5' \
     --schedule_url "https://docs.google.com/spreadsheets/d/1qkbOOzS-60WmSUVmWLwd7zpRBQI0fkaMeFrCXN_pyUk/export?format=csv&gid=1181436982"
 echo
@@ -38,6 +43,7 @@ echo
 echo "OLS-6"
 python bin/prepare_website_data.py \
     updateschedule \
+    -p "openseeds"  \
     --cohort '6' \
     --schedule_url "https://docs.google.com/spreadsheets/d/13rJ_Qqre7VfiW6Ioh-v4r6Xc9PvodZYriJ1ZzUJrRF8/export?format=csv&gid=1181436982"
 echo
@@ -45,6 +51,7 @@ echo
 echo "OLS-7"
 python bin/prepare_website_data.py \
     updateschedule \
+    -p "openseeds"  \
     --cohort '7' \
     --schedule_url "https://docs.google.com/spreadsheets/d/1jXJH8J4MKee80ODcgWoxpvPkV8ob0tw86Fov8nlXxXw/export?format=csv&gid=1181436982"
 echo
@@ -52,5 +59,6 @@ echo
 echo "OLS-8"
 python bin/prepare_website_data.py \
     updateschedule \
+    -p "openseeds"  \
     --cohort '8' \
     --schedule_url "https://docs.google.com/spreadsheets/d/1Tnk-kkHUmPOEfUOEAl627l6JS9SoY5D4gSAPCYoBRSY/export?format=csv&gid=2109933309"
