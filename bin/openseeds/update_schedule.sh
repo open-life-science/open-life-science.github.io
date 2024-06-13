28fe9d1fac5495e5ddfc751d51c4ee9f6ec78372
Deborah Udoh <deborahudoh02@gmail.com>
2024-06-13 13:51:25 +0000

    Temporarily use ols-8 schedule to populate ols-9.

--- bin/openseeds/update_schedule.sh
+++ bin/openseeds/update_schedule.sh
@@ -68,4 +68,4 @@ python bin/prepare_website_data.py \
     updateschedule \
     -p "openseeds"  \
     --cohort '9' \
-    --schedule_url ""
+    --schedule_url "https://docs.google.com/spreadsheets/d/1Tnk-kkHUmPOEfUOEAl627l6JS9SoY5D4gSAPCYoBRSY/export?format=csv&gid=2109933309"
